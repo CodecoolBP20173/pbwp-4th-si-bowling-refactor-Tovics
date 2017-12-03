e4c0c70204f40fdc0ff867d1fc83b6c5b78e8492
Tovics <tovics1@gmail.com>
2017-12-03 20:34:10 +0100

    basic refactoring

--- bowling.py
+++ bowling.py
@@ -1,3 +1,4 @@
+
 def score(game):
     result = 0
     frame = 1
@@ -30,10 +31,11 @@ def score(game):
             frame += 1
     return result
 
+
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+        char == '4' or char == '5' or char == '6' or \
+        char == '7' or char == '8' or char == '9':
         return int(char)
     elif char == 'X' or char == 'x':
         return 10
@@ -43,3 +45,10 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
